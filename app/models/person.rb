4afa63d219e9ffb859b70e2c8d62d4f0de7000fb
Maikel Chandika <maikel.chandika@vidio.com>
2020-08-29 09:17:31 +0700

    Replace manual rails form with bootstrap_form gem

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,10 @@
 class Person < ApplicationRecord
   enum gender: { male: 'male', female: 'female' }
+
+  validates :first_name, presence: true,
+                   length: { maximum: 50 }
+  validates :last_name, presence: true,
+                   length: { maximum: 50 }
+  validates :birth_date, :height, :weight, presence: true
+
 end
